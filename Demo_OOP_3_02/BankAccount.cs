a2680e183663ea7911830c567e8acf34709059fb
essamadel191 <essam.adel191@gmail.com>
2026-02-28 03:49:49 +0200

    Encapsulation & Property Types

---
+++ Demo_OOP_3_02/BankAccount.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo_OOP_3_02
+{
+    internal class BankAccount
+    {
+        #region Fields
+        
+        // _ means is a field
+        private int _AccountNumber;
+        private decimal _Balance;
+        private string _Name;
+
+        #endregion
+
+        #region Encapsulation Using Getter and Setter Methods
+
+        public decimal GetBalance()
+        {
+            return _Balance;
+        }
+
+        public void SetBalance(decimal balance)
+        {
+            if (balance < 0)
+            {
+                Console.WriteLine("Balance cannot be negative !");
+                return;
+            }
+            _Balance = balance;
+        }
+
+        public int GetAccountNumber() // Readonly
+        {
+            return _AccountNumber;
+        }
+
+        public void SetName(string name)
+        {
+            _Name = name;
+        }
+
+        #endregion
+
+        #region Encapsulation Using Properties
+
+        // Auto-implemented property
+        //public int Age { get; set; }
+        //private int _year;
+
+        //public int Year
+        //{
+        //    get { return _year; }
+        //    set { _year = value;}
+        //}
+
+        public string _name
+        {
+            get {  return _Name; }
+            set {
+                if(string.IsNullOrWhiteSpace(value) || value.Length < 3)
+                {
+                    Console.WriteLine("Invalid Name !");
+                    return;
+                }
+                _Name = value; 
+            }
+        }
+
+        #endregion
+
+        #region Property Types
+
+        #region Read/Write Property
+            
+            public int AccountNumber
+            {
+                get { return _AccountNumber; }
+                set { _AccountNumber = value; }
+            }
+
+        #endregion
+
+        #region Read-Only Property
+        public decimal Balance
+        {
+            get { return _Balance; }
+        }
+        #endregion
+
+        #region Write -Only Property
+
+        public string Name
+        {
+               set { _Name = value; }
+        }
+
+        #endregion
+
+        #region Automatic Property
+
+        // CLR will create a backing field for name
+
+        public string Name2 { get; set; }
+
+        #endregion
+
+        #region Property with Private Setter
+
+        public int pin { get; private set; }
+        #endregion
+
+        #region Computed Property
+
+       public bool HasBalance
+        {
+            get { return _Balance > 0; }
+        }
+
+        #endregion
+
+        #region Init Property
+
+        public int? Pin2 { get; init; }
+
+        #endregion
+
+        #region Expression-bodied Property
+        public int age { get; set; }
+        public int Year => DateTime.Now.Year - age;
+
+        #endregion
+
+        #endregion
+    }
+}
--- Demo_OOP_3_02/Program.cs
+++ Demo_OOP_3_02/Program.cs
@@ -3,5 +3,80 @@
     using System;
     public class Program
     {
+        private int _age;
+        static void Main(string[] args)
+        {
+            #region Encapsulation Using Getter and Setter Methods
+            //BankAccount Acc01 = new BankAccount();
+            //Acc01.SetBalance(-50); // invalid balance
+            //Console.WriteLine($"Balance: {Acc01.GetBalance()}");
+
+            //BankAccount Acc02 = new BankAccount();
+            //Acc02.SetBalance(-50); // valid balance
+            //Console.WriteLine($"Balance: {Acc02.GetBalance()}");
+            #endregion
+
+            #region Encapsulation Using Properties
+
+            //BankAccount Acc03 = new BankAccount();
+            //Acc03._name = "John Doe";
+            //Console.WriteLine($"Name: {Acc03._name}");
+
+            #endregion
+
+            #region Property Types
+
+            #region Read/Write Property
+            //BankAccount account = new BankAccount();
+            //account.AccountNumber = 123; // set
+            //Console.WriteLine($"Account Number: {account.AccountNumber}"); // get
+
+            #endregion
+
+            #region Read-Only Property
+
+            //BankAccount acc04 = new BankAccount();
+            ////acc04.Balance = 1000; // Error: Cannot set read-only property
+            //Console.WriteLine($"Balance: {acc04.Balance}"); // get
+
+            #endregion
+
+            #region Write-Only Property
+
+            //BankAccount account2 = new BankAccount();
+            //account2.Name = "Alice"; // set
+            ////Console.WriteLine($"Name: {account2.Name}"); // Error: Cannot get write-only property
+
+            #endregion
+
+            #region Property with Private Setter
+
+            //BankAccount account2 = new BankAccount();
+            ////account2.pin = 1234; // Error: Cannot set property with private setter
+            //Console.WriteLine($"PIN: {account2.pin}"); // get
+
+            #endregion
+
+            #region Computed Property
+
+            //BankAccount account2 = new BankAccount();
+            //account2.SetBalance(500); // Set balance using method
+            //Console.WriteLine($"Has Balance: {account2.HasBalance}"); // get computed property
+
+
+            #endregion
+
+            #region Init Property
+
+            //BankAccount account = new BankAccount { Pin2 = 5678 }; // set during initialization
+            ////account.Pin2 = 5678; // set during initialization
+            //Console.WriteLine($"PIN2: {account.Pin2}"); // get
+
+            #endregion
+
+
+
+            #endregion
+        }
     }
-}
+} 
